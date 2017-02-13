e9839c6a6aebc19f9ca4ac01d9f1fbef66f79d64
Xi Zhu <zhuxi28@hotmail.com>
2017-02-13 13:57:45 -0500

    angular error added

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import math
+# math.acos(0)
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 
@@ -97,36 +99,40 @@ def calc_ground_truth_score(outputA, outputB, labels):
 
 def sel_net_inference(input):
     # Common Convolutional Layers
-    # Layer 1: Input = 47x47x2, Output = 10x10x128
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(8,8,3,128), mean = mu, stddev = sigma))
-    conv1_b = tf.Variable(tf.zeros(128))
-    conv1 = tf.nn.conv2d(input, conv1_W, strides=(1,4,4,1), padding='VALID') + conv1_b
-    conv1 = tf.nn.relu(conv1)
-    # conv1 = tf.nn.max_pool(conv1, ksize=[], strides=[], padding='VALID')
+    with tf.name_scope('Sel_Conv_1') as scope:
+        # Layer 1: Input = 47x47x2, Output = 10x10x128
+        conv1_W = tf.Variable(tf.truncated_normal(shape=(8,8,3,128), mean = mu, stddev = sigma))
+        conv1_b = tf.Variable(tf.zeros(128))
+        conv1 = tf.nn.conv2d(input, conv1_W, strides=(1,4,4,1), padding='VALID') + conv1_b
+        conv1 = tf.nn.relu(conv1)
+        # conv1 = tf.nn.max_pool(conv1, ksize=[], strides=[], padding='VALID')
 
     # Layer 2: Input = , Output = 4x4x256
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(4,4,128,256), mean = mu, stddev = sigma))
-    conv2_b = tf.Variable(tf.zeros(256))
-    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=(1,2,2,1), padding='VALID') + conv2_b
-    conv2 = tf.nn.relu(conv2)
-    # conv2 = tf.nn.max_pool(conv2, ksize=[], strides=[], padding='VALID')
+    with tf.name_scope('Sel_Conv_2') as scope:
+        conv2_W = tf.Variable(tf.truncated_normal(shape=(4,4,128,256), mean = mu, stddev = sigma))
+        conv2_b = tf.Variable(tf.zeros(256))
+        conv2 = tf.nn.conv2d(conv1, conv2_W, strides=(1,2,2,1), padding='VALID') + conv2_b
+        conv2 = tf.nn.relu(conv2)
+        # conv2 = tf.nn.max_pool(conv2, ksize=[], strides=[], padding='VALID')
 
     # Flatten: Input = 4x4x256, Output = 4096
     fc0 = flatten(conv2)
+    with tf.name_scope('Sel_fc_1') as scope:
+        fc1_W = tf.Variable(tf.truncated_normal(shape=(4096, 256), mean = mu, stddev = sigma))
+        fc1_b = tf.Variable(tf.zeros(256))
+        fc1 = tf.matmul(fc0, fc1_W) + fc1_b
+        fc1 = tf.nn.relu(fc1)
 
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(4096, 256), mean = mu, stddev = sigma))
-    fc1_b = tf.Variable(tf.zeros(256))
-    fc1 = tf.matmul(fc0, fc1_W) + fc1_b
-    fc1 = tf.nn.relu(fc1)
-
-    fc2_W = tf.Variable(tf.truncated_normal(shape=(256, 2), mean = mu, stddev = sigma))
-    fc2_b = tf.Variable(tf.zeros(2))
-    logits = tf.matmul(fc1, fc2_W) + fc2_b
+    with tf.name_scope('Sel_fc_2') as scope:
+        fc2_W = tf.Variable(tf.truncated_normal(shape=(256, 2), mean = mu, stddev = sigma))
+        fc2_b = tf.Variable(tf.zeros(2))
+        logits = tf.matmul(fc1, fc2_W) + fc2_b
 
     return logits
 
 def sel_net_loss(logits, labels):
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, labels)
+    with tf.name_scope('Sel_Loss') as scope:
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, labels)
     return tf.reduce_mean(cross_entropy)
 
 def sel_net_training(loss, learning_rate):
@@ -138,3 +144,46 @@ def sel_net_training(loss, learning_rate):
 def sel_net_evaluation(logits, labels):
     cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, labels)
     return tf.reduce_mean(cross_entropy)
+
+#####################################################################################
+
+def least_square_error(output, labels):
+    with tf.name_scope('Least_square_error') as scope:
+        loss = tf.reduce_mean(tf.square(tf.sub(output, labels), name="Error"))
+    return loss
+
+def num(output, labels):
+    numeratro =  tf.reduce_sum(tf.mul(output, labels), axis=1)
+    return numeratro
+
+def denom(output, labels):
+    denominator = tf.mul(tf.sqrt(tf.reduce_sum(tf.mul(output, output), axis=1)), \
+                             tf.sqrt(tf.reduce_sum(tf.mul(labels, labels), axis=1)))
+    return denominator
+
+
+def angular_error(output, labels):
+    with tf.name_scope('Angular_error') as scope:
+        numeratro = num(output, labels)
+        denominator = denom(output, labels)
+        loss = tf.reduce_mean(tf.acos(tf.div(numeratro, denominator)))      
+    return loss
+
+def inference(outputA, outputB, output_sel):
+    '''
+    For test, use output from A, B hyp-net and score from sel-net to decide the
+    final output of the whole network. 
+    Compare the final output with labels.
+    '''
+    with tf.name_scope("Inference") as scope:
+        output = tf.select(output_sel[:, 0] > output_sel[:, 1], outputA, outputB)
+    return output
+
+def evaluation(output, labels):
+    with tf.name_scope("Evaluation") as scope:
+        # loss = least_square_error(output, labels)
+        loss = angular_error(output, labels)
+    return loss
+
+
+
--- train.py
+++ train.py
@@ -13,7 +13,7 @@ FLAGS = None
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
-learning_rate = 0.001
+learning_rate = 0.0001
 epochs = 30
 batch_size = 32
 
@@ -21,16 +21,24 @@ def train_hyp_net(images, labels):
     assert(len(images) == len(labels))
     run_name = datetime.now().strftime("%I:%M%p on %B %d, %Y")
     train_X, test_X, train_y, test_y = train_test_split(images, labels, test_size=0.2, random_state=0)
-    # train_X, val_X, train_y, val_y = train_test_split(train_X, train_y, test_size=0.2, random_state=0)
+    train_X, val_X, train_y, val_y = train_test_split(train_X, train_y, test_size=0.2, random_state=0)
+    learning_rate = FLAGS.learning_rate
 
     num_examples = len(train_X)
     num_batches_per_epoch = len(train_X) // FLAGS.batch_size
     # decay_steps = int(num_batches_per_epoch * FLAGS.num_epochs_per_decay)
     # print ("{0} {1} {2}".format(num_examples, num_batches_per_epoch, num_batches_per_epoch))
-    
-    with tf.Graph().as_default():
+    print("learning rate: {0}".format(learning_rate))
 
-        learning_rate = FLAGS.init_learning_rate
+    #### early stop
+
+    non_improve_count = 0
+    min_error = sys.maxint
+    early_stop_threshold = FLAGS.early_stop_threshold
+    ####
+
+    with tf.Graph().as_default():
+        
         global_step = tf.get_variable('global_step', [], trainable=False,
                                       initializer=tf.constant_initializer(0))
         # learning_rate = tf.train.exponential_decay(FLAGS.initial_learning_rate,
@@ -41,25 +49,29 @@ def train_hyp_net(images, labels):
 
         X = tf.placeholder(tf.float32, (None, IMG_SIZE, IMG_SIZE, 3), name="Images")
         y = tf.placeholder(tf.float32, (None, 2), name="Labels")
+        ground_truth_score = tf.placeholder(tf.float32, (None, 2), name="Ground_Truth_Score")
         
         outputA, outputB = hyp_net_inference(X)
         hyp_loss = hyp_net_loss(outputA, outputB, y)
         hyp_train_op = hyp_net_training(hyp_loss, learning_rate)
-        eval_correct = hyp_net_evaluation(outputA, outputB, y)
-        ground_truth_score = calc_ground_truth_score(outputA, outputB, y)
+        hyp_eval_correct = hyp_net_evaluation(outputA, outputB, y)
+        calc_gt_score = calc_ground_truth_score(outputA, outputB, y)
         
         output = sel_net_inference(X)
         sel_loss = sel_net_loss(output, ground_truth_score)
         sel_train_op = sel_net_training(sel_loss, learning_rate)
-        eval_correct = sel_net_loss(output, y)
+        sel_eval_correct = sel_net_loss(output, ground_truth_score)
+
+        final_infer = inference(outputA, outputB, output, y)
 
         init = tf.global_variables_initializer()
         sess = tf.Session()
         saver = tf.train.Saver()
         
         # Summaries
-        tf.summary.scalar("Training_Loss", hyp_loss)
-        # tf.summary.scalar("Validation_Loss", eval_correct)
+        tf.summary.scalar("Hyp_Training_Loss", hyp_loss)
+        tf.summary.scalar("Sel_Training_Loss", sel_loss)
+        # tf.summary.scalar("Hyp_Validation_Loss", hyp_eval_correct)
 
         # train_X = hyp_net_pre_process(train_X)
         with tf.Session() as sess:
@@ -69,38 +81,61 @@ def train_hyp_net(images, labels):
             print ('Training HypNet...')
             for i in range(FLAGS.epochs):
                 train_X, train_y = shuffle(train_X, train_y)
-                print("Epoch: %d" % i)
-                for j in range(0, num_batches_per_epoch):
+                print("Epoch: %d" % (i + 1))
+                for j in range(num_batches_per_epoch):
                     offset = j * FLAGS.batch_size
                     start_time = time.time()
                     batch_X, batch_y = train_X[offset:offset + FLAGS.batch_size], train_y[offset:offset + FLAGS.batch_size]
-                    _, loss_value, summary = sess.run([hyp_train_op, hyp_loss, merged], feed_dict={X: batch_X, y: batch_y})
+                    _, loss_value, summary = sess.run([hyp_train_op, hyp_loss, merged], feed_dict={X: batch_X, y: batch_y, ground_truth_score: batch_y})
                     duration = time.time() - start_time
-                    print("Training loss: %s; Takes %d" % (loss_value, duration))
-                # loss_value = sess.run(eval_correct, feed_dict={X:val_X, y:val_y})
-                # print("Validation loss: %s" % (loss_value))
+                    print("Hyp Training loss: %s" % (loss_value))
+                loss_value = sess.run(hyp_eval_correct, feed_dict={X:val_X, y:val_y})
+                loss_value = loss_value.item() # convert to float
+                print("Hyp Validation loss: %f" % (loss_value))
+                
+                if loss_value < min_error:
+                    min_error = loss_value
+                else:
+                    non_improve_count += 1
+                if non_improve_count > FLAGS.early_stop_threshold:
+                    break
+
                 summary_writer.add_summary(summary, i)
 
-            saver.save(sess, './hyp_net')
-            summary_writer.close()
-            
-            loss_value = sess.run(eval_correct, feed_dict={X:test_X, y:test_y})
+            loss_value = sess.run(hyp_eval_correct, feed_dict={X:test_X, y:test_y})
             print("Finish training HypNet. Evaluation loss: %s" % loss_value)
-            
-            # gt_score = sess.run(ground_truth_score, feed_dict={X:train_X, y:train_y})
-            # print ('Training SelNet...')
-            # for i in range(FLAGS.epochs):
-            #     train_X, train_y = shuffle(train_X, train_y)
-            #     print("Epoch: %d" % i)
-            #     for j in range(0, num_batches_per_epoch):
-            #         offset = j * FLAGS.batch_size
-            #         start_time = time.time()
-            #         batch_X, batch_y = train_X[offset:offset + FLAGS.batch_size], train_y[offset:offset + FLAGS.batch_size]
-            #         _, loss_value, summary = sess.run([hyp_train_op, hyp_loss, merged], feed_dict={X: batch_X, y: batch_y})
-            #         duration = time.time() - start_time
-            #         print("Loss: %s; Takes %d" % (loss_value, duration))
-            #     summary_writer.add_summary(summary, i)
 
+            gt_score_train = sess.run(calc_gt_score, feed_dict={X:train_X, y:train_y})
+            gt_score_val = sess.run(calc_gt_score, feed_dict={X:val_X, y:val_y})
+
+            print ('Training SelNet...')
+            for i in range(FLAGS.epochs):
+                train_X, gt_score_train = shuffle(train_X, gt_score_train)
+                print("Epoch: %d" % (i + 1))
+                for j in range(num_batches_per_epoch):
+                    offset = j * FLAGS.batch_size
+                    start_time = time.time()
+                    batch_X, batch_gt_score = train_X[offset:offset + FLAGS.batch_size], gt_score_train[offset:offset + FLAGS.batch_size]
+                    _, loss_value, summary = sess.run([sel_train_op, sel_loss, merged], feed_dict={X: batch_X, y: batch_gt_score, ground_truth_score: batch_gt_score})
+                    duration = time.time() - start_time
+                    print("Sel Training Loss: %s" % (loss_value))
+                loss_value = sess.run(hyp_eval_correct, feed_dict={X:val_X, y:val_y})
+                loss_value = loss_value.item() # convert to float
+                print("Hyp Validation loss: %f" % (loss_value))
+
+                if loss_value < min_error:
+                    min_error = loss_value
+                else:
+                    non_improve_count += 1
+                if non_improve_count > FLAGS.early_stop_threshold:
+                    break
+
+                summary_writer.add_summary(summary, i)
+            # if pass:
+                # saver.save(sess, './hyp_net')
+            final_loss = sess.run(final_infer, feed_dict={X: test_X, y: test_y})
+            print("Test loss: %s" % (final_loss))
+            summary_writer.close()
 
 
 def main(_):
@@ -112,7 +147,7 @@ if __name__ == "__main__":
     # Sequence of training between the two networks?
     parser = argparse.ArgumentParser()
     parser.add_argument(
-      '--init_learning_rate',
+      '--learning_rate',
       type=float,
       default=0.01,
       help='Initial learning rate.'
@@ -128,8 +163,15 @@ if __name__ == "__main__":
     parser.add_argument(
       '--batch_size',
       type=int,
-      default=128,
-      help='Number of steps to run trainer.'
+      default=64,
+      help='Number of data item in a batch.'
+    )
+
+    parser.add_argument(
+      '--early_stop_threshold',
+      type=int,
+      default=5,
+      help='Number of epoch without improvement to early stop'
     )
 
     FLAGS, unparsed = parser.parse_known_args()
